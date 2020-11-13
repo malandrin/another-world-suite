9b0c45f483d3b0d497521732ddaaf5c1c29af4a0
César Botana <cesar@hotmail.com>
2020-11-13 19:00:50 +0100

    set the right path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/domino-party/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/another-world-suite/' : '/',
   chainWebpack: config => {
     config.module
       .rule('wasm')
